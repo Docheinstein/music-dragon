9df8d0f3ffda1ceca8b3b9ec5b2380b7da8d8063
Docheinstein <dottorestefano97@gmail.com>
2022-07-09 16:17:07 +0200

    Update to PyPi: v 0.9

--- music_dragon/__init__.py
+++ music_dragon/__init__.py
@@ -1,7 +1,7 @@
 APP_NAME = "music-dragon"
 APP_DISPLAY_NAME = "MusicDragon"
 APP_ORGANIZATION_NAME = "Docheinstein"
-APP_VERSION = "0.8"
+APP_VERSION = "0.9"
 
 UNKNOWN_ARTIST = "Unknown Artist"
 UNKNOWN_ALBUM = "Unknown Album"
