ef6d2fcbe7537ead136faec37f3c8476ffd78424
Docheinstein <dottorestefano97@gmail.com>
2024-08-29 16:30:35 +0200

    Version 0.14

--- music_dragon/__init__.py
+++ music_dragon/__init__.py
@@ -1,7 +1,7 @@
 APP_NAME = "music-dragon"
 APP_DISPLAY_NAME = "MusicDragon"
 APP_ORGANIZATION_NAME = "Docheinstein"
-APP_VERSION = "0.13"
+APP_VERSION = "0.14"
 
 UNKNOWN_ARTIST = "Unknown Artist"
 UNKNOWN_ALBUM = "Unknown Album"
